acdfe8befab4638b394e23543f075acdea2b6298
Christoph Wübbels <c.wuebbels@jobtender24.com>
2022-05-23 12:57:48 +0000

    Downgrade support for django >3.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'Django>=3.2',
+        'Django>=3.0',
         'python-dateutil>=2.1',
         'pyopenssl>=0.13.1',
         'pem>=16.0.0',
